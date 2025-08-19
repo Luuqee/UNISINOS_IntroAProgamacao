1d53c13631b8d819101389ffa42f0ef007d01839
Lucas Souza <www.luucas@gmail.com>
2025-08-19 16:39:35 -0300

    Exercicio 4 e 5

---
+++ Semana 1 e 2/Exercicio 5.py
@@ -0,0 +1,11 @@
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+c = int(input("Digite o valor de c: "))
+d = int(input("Digite o valor de d: "))
+e = int(input("Digite o valor de e: "))
+
+soma = a+b+c+d+e
+produto = a*b*c*d*e
+
+print("A soma dos números é:", soma)
+print("O produto dos números é:", produto)
